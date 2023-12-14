6bdc389e4068926112e906561b1a82582595c57a
root <root@node159557-dockerswarm.jelastic.saveincloud.net>
2023-12-14 15:37:07 +0000

    Add new python script for check if the repository /website-portifolio are diferent from remote to local, automatically update the website folders and re-deploy website

--- lenda/scripts/get-last-commit.py
+++ lenda/scripts/get-last-commit.py
@@ -4,7 +4,7 @@ import os
 
 
 username = "yohrannes" # substitua pelo nome de usuário do GitHub
-repo_name = "docker-lenda" # substitua pelo nome do repositório
+repo_name = "website-portifolio" # substitua pelo nome do repositório
 
 url = f"https://api.github.com/repos/{username}/{repo_name}/commits"
 response = requests.get(url)
@@ -12,18 +12,19 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
     latest_commit_sha = data[0]['sha']
-    actual_commit_sha = f"{latest_commit_sha}"
-    # latest_commit_sha = "teste"
-    print(f"{actual_commit_sha}")
-    print(f"{latest_commit_sha}")
+    workdir = "/root/repos-github/website-portifolio"
+    os.chdir(workdir)
+    actual_commit_sha = os.popen('git rev-parse HEAD').read().strip()
+    print(f"{actual_commit_sha}", "-> local website commit")
+    print(f"{latest_commit_sha}", "-> latest website commit on github")
 else:
     print(f"{response.status_code}")
     
 if actual_commit_sha != latest_commit_sha:
-    print ("commit atualizado")
-else:
-    print ("commit em dia")
-    workdir = "/root/rep-github/website-portifolio"
-    os.chdir(workdir)
-    command = "git pull origin main"
+    print ("updating website folders....")
+    workdir = "/root/repos-github/website-portifolio"
+    os.chdir(workdir)x'
+    command = "sudo lsof -t -i tcp:5000 | xargs sudo kill -9;git pull origin main;python3 app.py"
     os.system(command)
+else:
+    print ("commits up-to-date")
