e7d1e8bc49a9ce4ea9b9da13dd707d5a848d4686
root <root@node162832-lenda.jelastic.saveincloud.net>
2023-12-14 09:16:30 +0000

    fix issues

--- lenda/scripts/enable-start-docker.sh
+++ lenda/scripts/enable-start-docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sudo systemctl start docker
-sudo systemctl enable docker
+sudo systemctl stop docker
+sudo systemctl disable docker
+sudo yum remove -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum remove -y docker-ce docker-ce-cli containerd.io
